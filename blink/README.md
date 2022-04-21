095054bdc1d54961065b81023c2496de618f5857
Alberto Panu <alberto@panu.it>
2022-04-21 11:13:26 +0200

    change example

--- blink/README.md
+++ blink/README.md
@@ -1,3 +1,3 @@
-`blink.hex` is a program that blinks a led connected to **P1.2**. The cathode must be connected to the port, the anode to a 220 Ohm resistor connected to +5V.
+`blink.hex` is a program that blinks the leds connected to all P2 and P3 ports. The cathode must be connected to the port, the anode to a 330 Ohm resistor connected to +5V.
 
 `lorem.hex` is a sample text of 2048 bytes, the same of AT89S8252 data space, for test data write and read.
--- config.py
+++ config.py
@@ -4,10 +4,13 @@
 serialPort = 'COM28'
 
 ## Code Space
-#code_targetFile = 'blink/blink.hex'
-code_targetFile = 'blink/eidefirst.hex'
+# file used for chip programming
+code_targetFile = 'blink/blink.hex'
+# file used for save chip read programming content
 code_dumpFile   = 'blink/programdump.hex' 
 
 ## Data Space
+# file used for chip programming
 data_targetFile = 'blink/lorem.hex'
+# file used for save chip read data content
 data_dumpFile   = 'blink/datadump.hex'
